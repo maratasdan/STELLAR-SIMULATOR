32ebea308978497c490b4da7c7d7188a3d8d1474
MARIO MARATAS JR <danyoumaratas@gmail.com>
2026-06-25 08:34:26 +0800

    Initial Commit

---
+++ STELLAR SIMULATOR/STELLAR_SIMULATORApp.swift
@@ -0,0 +1,17 @@
+//
+//  STELLAR_SIMULATORApp.swift
+//  STELLAR SIMULATOR
+//
+//  Created by Danxd on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct STELLAR_SIMULATORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
