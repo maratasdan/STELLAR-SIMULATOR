93bab160894ce834e612505dbfc3dd4948536512
MARIO MARATAS JR <danyoumaratas@gmail.com>
2026-06-26 08:44:30 +0800

    June 26, 2026

---
+++ STELLAR SIMULATOR/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  STELLAR SIMULATOR
+//
+//  Created by Danxd on 6/25/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ STELLAR SIMULATOR/Station.swift
@@ -0,0 +1,8 @@
+//
+//  Station.swift
+//  STELLAR SIMULATOR
+//
+//  Created by Danxd on 6/25/26.
+//
+
+import Foundation
